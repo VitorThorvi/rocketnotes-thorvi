dafa2839a938512c728506cc9d1bb4a2e7f8d909
Vitor Thorvi <euvitoreu@gmail.com>
2022-10-17 09:49:49 -0300

    adds new note page

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,10 +1,39 @@
-import {Container} from "./styles.js";
+import {Container, Form} from "./styles.js";
 import {Header} from "../../components/Header/index.jsx";
+import {Input} from "../../components/Input/index.jsx";
+import {Textarea} from "../../components/Textarea/index.jsx";
+import {NoteItem} from "../../components/NoteItem/index.jsx";
+import {Section} from "../../components/Section/index.jsx";
+import {Button} from "../../components/Button/index.jsx";
 
-export function New () {
-  return(
+export function New() {
+  return (
     <Container>
-      <Header />
+      <Header/>
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título"/>
+          <Textarea placeholder="Observações"/>
+          <Section title="Links úteis">
+            <NoteItem value="https://rocketseat.com.br"/>
+            <NoteItem isNew placeholder="Novo link"/>
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react"/>
+              <NoteItem isNew placeholder="Nova tag"/>
+            </div>
+          </Section>
+
+          <Button xernobis="Salvar"/>
+        </Form>
+      </main>
     </Container>
   )
 }
